0e09f55ca6a37f707053a9ec7271a0a70fd5197b
Kyle Alzayadi <kylealzayadi@Kyles-MacBook-Pro.local>
2018-07-10 20:28:07 -0700

    doesnt work because of latlng

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { initDomAdapter } from '../../../node_modules/@angular/platform-browser/src/browser';
+import { GESTURE_PRIORITY_TOGGLE } from '../../../node_modules/ionic-angular/umd/gestures/gesture-controller';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+@ViewChild('map') mapElement;
+map: any;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+
+ionViewDidLoad(){
+  this.initMap();
+}
+
+initMap(){
+  let latLng = new google.maps.LatLng(-34,0224, 118,2851);
+  
+  let mapOptions = {
+    center: latLng,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+}
 
 }
